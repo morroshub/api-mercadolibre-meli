0a42dc9b2979da67b0f1f547431f6978f82f9da5
morroshub <lgmorrone@icloud.com>
2024-01-02 13:37:18 -0300

    se instalaron dependencias, se configuro el servidor

---
+++ server.ts
@@ -0,0 +1,32 @@
+//imports
+
+import express, {Express, Request, Response} from "express";
+import * as dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors"
+
+dotenv.config(); //variable de entorno
+
+const PORT = parseInt(process.env.PORT as string, 10) || 4000;
+
+// express
+const server: Express = express()
+
+//server
+server.use(express.json())
+server.use(express.urlencoded({extended: true}))
+server.use(cors())
+server.use(helmet())
+
+
+
+//import router
+server.get("/", (req: Request, res: Response) => {
+    res.send(`[server]: Server is running at http://localhost:${PORT}`);
+})
+
+
+//puertos abiertos
+server.listen(PORT, ()=> {
+    console.log(`[server]: Server is running at http://localhost:${PORT}`);
+})
